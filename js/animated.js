fffe1919636774e3a4305a46fc8632acbc90ff96
Silvio <silvio.dias@etec.sp.gov.br>
2021-07-17 02:50:21 -0300

    add animated.js

---
+++ js/animated.js
@@ -0,0 +1,12 @@
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '50px',
+  duration: 2000,
+  reset: true
+});
+
+ScrollReveal().reveal('#banner', { delay: 200 });
+ScrollReveal().reveal('.main h1', { delay: 400 });
+ScrollReveal().reveal('.countdown', { delay: 600 });
+ScrollReveal().reveal('.countdown-legend', { delay: 700 });
+ScrollReveal().reveal('.form-content', { delay: 750 });
